24d6faf2ea18c8ade295e9b90c6e827e51a64d7e
Luca cornaglia <lcornag@gmail.com>
2018-03-01 15:07:10 +0100

    "parallax.js añadido"

---
+++ assets/js/parallaxInicio.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    parallax();
+});
+function parallax(){
+    var verticalScroll = $(window).scrollTop();
+
+
+    $('.parallax--bg').css('background-position',
+        'center '+(verticalScroll*0.5)+'px')
+}
